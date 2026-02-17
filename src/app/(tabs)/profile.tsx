89c5edb4d0c965ba7fbbd3937281827afe878648
Nupur Trivedi <nupur.trivedi@srkay.com>
2026-02-17 16:56:42 +0530

    setup basic navigation repo

---
+++ src/app/(tabs)/profile.tsx
@@ -0,0 +1,24 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Profile() {
+    return(
+        <View>
+            <Text>On Profile Screen</Text>
+            <Link href="/"></Link>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {
+        fontSize: 20,
+        textDecorationLine: 'underline',
+    }
+})
